8b047ad0cf9e0d29745fa2157ac948ebe01e54ae
Richard dos Santos Otto <shoonkey.dev@gmail.com>
2023-09-16 21:20:51 -0300

    chore: turned setup args to object for readability

--- src/setupApp.tsx
+++ src/setupApp.tsx
@@ -2,14 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 
-import theme from "./chakra/theme.ts";
-import App from "./App.tsx";
+import theme from "./chakra/theme";
+import App from "./App";
 
-function setupApp(
-  containerId: string,
-  basename: string = "/",
-  isSubApp: boolean = false
-) {
+interface AppSetupProps {
+  containerId: string;
+  basename?: string;
+  isSubApp?: boolean;
+}
+
+function setupApp({
+  containerId,
+  basename = "/",
+  isSubApp = false,
+}: AppSetupProps) {
   ReactDOM.createRoot(document.getElementById(containerId)!).render(
     <React.StrictMode>
       <ChakraProvider theme={theme}>
