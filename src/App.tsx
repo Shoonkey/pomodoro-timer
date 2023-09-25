73514f02eaed7fea3d972af600b7ecd82204a1ec
Richard dos Santos Otto <shoonkey.dev@gmail.com>
2023-09-24 21:16:09 -0300

    chore: moved Root to its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,135 +1,27 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { useMemo } from "react";
+import { useTranslation, I18nextProvider } from "react-i18next";
 import {
-  Box,
-  ChakraProvider,
-  Flex,
-  IconButton,
-  Select,
-  Tooltip,
-  VisuallyHidden,
-} from "@chakra-ui/react";
-import {
-  Route,
-  RouterProvider,
-  Routes,
-  createBrowserRouter,
   createHashRouter,
-  useLocation,
-  useNavigate,
+  createBrowserRouter,
+  RouterProvider,
 } from "react-router-dom";
-import { I18nextProvider, useTranslation } from "react-i18next";
-import { useEffect, useMemo } from "react";
-import { SunHorizon, MoonStars } from "@phosphor-icons/react";
 
-import AppSettingsProvider, {
-  AppSettingsData,
-} from "./common/components/AppSettingsProvider";
-import useAppSettings from "./common/hooks/useAppSettings";
+import AppSettingsProvider from "./common/components/AppSettingsProvider";
+import Root from "./common/components/Root";
 import setupI18N from "./common/i18n";
-import AppV1 from "./v1/App";
-import AppV2 from "./v2/App";
 
-function Root() {
-  const { t, i18n } = useTranslation();
-  const { isSubapp, theme, setTheme } = useAppSettings();
-  // const location = useLocation();
-  // const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   if (location.pathname === "/") {
-  //     navigate("/v2");
-  //     return;
-  //   }
-  // }, [location]);
-
-  return (
-    <Flex
-      flexDir="column"
-      w={isSubapp ? "100%" : "100dvw"}
-      h={isSubapp ? "100%" : "100dvh"}
-    >
-      <Flex justifyContent="end" alignItems="center" p={2}>
-        <Box
-          bg={
-            // TODO: When v2 comes around, leave it set as the gradient as there will always be a child element
-            isSubapp
-              ? "none"
-              : "linear-gradient(90deg, hsla(139, 72%, 83%, 1) 0%, hsla(229, 89%, 62%, 1) 100%);"
-          }
-          p={1}
-          borderRadius="8px"
-        >
-          <Flex gap={2} borderRadius="8px">
-            <VisuallyHidden>{t("appSettings")}</VisuallyHidden>
-            {/* <Select
-                bg="gray.800"
-                color="whiteAlpha.800"
-                w="auto"
-                aria-label={t("selectVersion")}
-                value={location.pathname.startsWith("/v1") ? "v1" : "v2"}
-                onChange={(e) => navigate(`/${e.target.value}`)}
-              >
-                <option value="v1">v1</option>
-                <option value="v2">v2</option>
-              </Select> */}
-            {!isSubapp && (
-              <>
-                <Select
-                  w="auto"
-                  bg="gray.800"
-                  color="whiteAlpha.800"
-                  aria-label={t("selectLanguage")}
-                  value={i18n.language}
-                  onChange={(e) => i18n.changeLanguage(e.target.value)}
-                >
-                  <option value="en-US">en-US</option>
-                  <option value="pt-BR">pt-BR</option>
-                  <option value="es-ES">es-ES</option>
-                </Select>
-                <Tooltip placement="left" label={t("changeTheme")}>
-                  <IconButton
-                    aria-label={t("changeTheme")}
-                    color="gray.800"
-                    bg="transparent"
-                    _hover={{ bg: "whiteAlpha.400" }}
-                    onClick={() =>
-                      setTheme(theme === "dark" ? "light" : "dark")
-                    }
-                    icon={
-                      theme === "dark" ? (
-                        <SunHorizon size={36} weight="fill" />
-                      ) : (
-                        <MoonStars size={36} weight="fill" />
-                      )
-                    }
-                  />
-                </Tooltip>
-              </>
-            )}
-          </Flex>
-        </Box>
-      </Flex>
-      <Flex flexGrow={1}>
-        <AppV1 />
-        {/* <Routes>
-            <Route
-              path="/v1/*"
-              element={<AppV1 />}
-            />
-            <Route
-              path="/v2/*"
-              element={<AppV2 />}
-            />
-          </Routes> */}
-      </Flex>
-    </Flex>
-  );
+interface AppProps {
+  isSubapp?: boolean;
+  theme?: "dark" | "light";
+  language?: string;
 }
 
 function App({
   isSubapp = false,
   theme = "dark",
   language = "pt-BR",
-}: AppSettingsData) {
+}: AppProps) {
   const i18nInstance = useMemo(() => setupI18N(language), [language]);
   const { i18n } = useTranslation(undefined, { i18n: i18nInstance });
 
---
+++ src/common/components/Root.tsx
@@ -0,0 +1,123 @@
+import {
+  Box,
+  Flex,
+  IconButton,
+  Select,
+  Tooltip,
+  VisuallyHidden,
+} from "@chakra-ui/react";
+import {
+  Route,
+  RouterProvider,
+  Routes,
+  createBrowserRouter,
+  createHashRouter,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { useEffect, useMemo } from "react";
+import { SunHorizon, MoonStars } from "@phosphor-icons/react";
+
+import useAppSettings from "../hooks/useAppSettings";
+import AppV1 from "../../v1/App";
+import AppV2 from "../../v2/App";
+
+function Root() {
+  const { t, i18n } = useTranslation();
+  const { isSubapp, theme, setTheme } = useAppSettings();
+  // const location = useLocation();
+  // const navigate = useNavigate();
+
+  // useEffect(() => {
+  //   if (location.pathname === "/") {
+  //     navigate("/v2");
+  //     return;
+  //   }
+  // }, [location]);
+
+  return (
+    <Flex
+      flexDir="column"
+      w={isSubapp ? "100%" : "100dvw"}
+      h={isSubapp ? "100%" : "100dvh"}
+    >
+      <Flex justifyContent="end" alignItems="center" p={2}>
+        <Box
+          bg={
+            // TODO: When v2 comes around, leave it set as the gradient as there will always be a child element
+            isSubapp
+              ? "none"
+              : "linear-gradient(90deg, hsla(139, 72%, 83%, 1) 0%, hsla(229, 89%, 62%, 1) 100%);"
+          }
+          p={1}
+          borderRadius="8px"
+        >
+          <Flex gap={2} borderRadius="8px">
+            <VisuallyHidden>{t("appSettings")}</VisuallyHidden>
+            {/* <Select
+                bg="gray.800"
+                color="whiteAlpha.800"
+                w="auto"
+                aria-label={t("selectVersion")}
+                value={location.pathname.startsWith("/v1") ? "v1" : "v2"}
+                onChange={(e) => navigate(`/${e.target.value}`)}
+              >
+                <option value="v1">v1</option>
+                <option value="v2">v2</option>
+              </Select> */}
+            {!isSubapp && (
+              <>
+                <Select
+                  w="auto"
+                  bg="gray.800"
+                  color="whiteAlpha.800"
+                  aria-label={t("selectLanguage")}
+                  value={i18n.language}
+                  onChange={(e) => i18n.changeLanguage(e.target.value)}
+                >
+                  <option value="en-US">en-US</option>
+                  <option value="pt-BR">pt-BR</option>
+                  <option value="es-ES">es-ES</option>
+                </Select>
+                <Tooltip placement="left" label={t("changeTheme")}>
+                  <IconButton
+                    aria-label={t("changeTheme")}
+                    color="gray.800"
+                    bg="transparent"
+                    _hover={{ bg: "whiteAlpha.400" }}
+                    onClick={() =>
+                      setTheme(theme === "dark" ? "light" : "dark")
+                    }
+                    icon={
+                      theme === "dark" ? (
+                        <SunHorizon size={36} weight="fill" />
+                      ) : (
+                        <MoonStars size={36} weight="fill" />
+                      )
+                    }
+                  />
+                </Tooltip>
+              </>
+            )}
+          </Flex>
+        </Box>
+      </Flex>
+      <Flex flexGrow={1}>
+        <AppV1 />
+        {/* <Routes>
+            <Route
+              path="/v1/*"
+              element={<AppV1 />}
+            />
+            <Route
+              path="/v2/*"
+              element={<AppV2 />}
+            />
+          </Routes> */}
+      </Flex>
+    </Flex>
+  );
+}
+
+export default Root;
